b0b4beddfef8bf9fec8afef0b6083e72e90d51bd
Rémi Labelle <remilabelle@outlook.com>
2022-09-01 10:02:31 -0400

    Implémentation des fonctions dans exercice.py

--- exercice.py
+++ exercice.py
@@ -9,27 +9,30 @@ def square_root(a: float) -> float:
 
 
 def square(a: float) -> float:
-    return 0.0
+    return a**2
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    return (a + b + c) / 3
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    return (angle_degs + angle_mins / 60 + angle_secs / 3600) * math.pi / 180
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
+    angle_degs = angle_rads / (math.pi / 180)
+    angle_mins = (angle_degs % 1) * 60
+    angle_secs = (angle_mins % 1) * 60
+    return angle_degs - angle_degs % 1, angle_mins - angle_mins % 1, angle_secs
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return (temperature - 32) / 1.8
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return temperature * 1.8 + 32
 
 
 def main() -> None:
